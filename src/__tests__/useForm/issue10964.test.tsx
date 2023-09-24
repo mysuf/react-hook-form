6a5f8ca9f52adad0a88937c324e6ce53938d534b
bigless <petr.schuchmann@gmail.com>
2023-09-24 19:56:59 +0200

    reproduces maximum deph error

--- src/__tests__/useForm/issue10964.test.tsx
+++ src/__tests__/useForm/issue10964.test.tsx
@@ -11,29 +11,33 @@ import { FormProvider, useFormContext } from '../../useFormContext';
 describe('formState', () => {
   it('should properly set isDirty when disabled is dynamicaly changed', async () => {
     const spyError = jest.spyOn(console, 'error');
-    let isDirty: null | boolean = null;
+    let isFormDirty: null | boolean = null;
     const defaultValues = { name: 'initial', disableName: true };
   
     const FieldList = () => {
-      const { formState, register, watch, reset } = useFormContext();
+      const { formState: { isDirty, dirtyFields },  register, watch, reset } = useFormContext();
       const disableName = watch('disableName');
-      isDirty = formState.isDirty;
+      isFormDirty = isDirty;
 
       // we haven't form data so we won't render fields
       if (disableName === undefined) {
         return (
-          <input
+          <button
             type="button"
+            data-testid="fetch-btn"
             onClick={() => reset(defaultValues)}
-            value="Emulate fetch form values"
-          />
+          >
+            Emulate fetch form values
+          </button>
         );
       }
 
       return (
         <>
-          <input type="text" {...register('name', { disabled: disableName })} data-testid="text-input" />
-          <input type="checkbox" {...register('disableName')} />
+          <input type="text" {...register('name', {  disabled: disableName })} data-testid="text-input" />
+          <div>Text input dirty: {dirtyFields.name ? "true" : "false"}</div>
+          <input type="checkbox" {...register('disableName', { disabled: false })} />
+          <div>Checkbox dirty: {dirtyFields.disableName ? "true" : "false"}</div>
         </>
       );      
     }
@@ -52,12 +56,12 @@ describe('formState', () => {
 
     render(<App />);
   
-    const reset = screen.getByRole('button');
+    const reset = screen.getByTestId("fetch-btn");
       
     // FAILS
-    // user didn't touch single field yet so isDirty should be false
+    // user didn't touch single field yet so isFormDirty should be false
     // lets skip it to get further
-    //expect(isDirty).toBe(false);
+    //expect(isFormDirty).toBe(false);
 
     fireEvent.click(reset);
 
@@ -74,25 +78,26 @@ describe('formState', () => {
       expect(spyError).not.toHaveBeenCalled();
 
       // FAILS
-      // user didn't touch single field yet so isDirty should be false
-      expect(isDirty).toBe(false);
+      // user didn't touch single field yet so isFormDirty should be false
+      expect(isFormDirty).toBe(false);
     });
+
 
     // now user changes value first time
     fireEvent.click(checkbox);
 
-    // checkbox was clicked and its value differs from defaultValue so isDirty should be true
+    // checkbox was clicked and its value differs from defaultValue so isFormDirty should be true
     await waitFor(() => {
       expect(checkbox).not.toBeChecked();
       expect(textInput).not.toBeDisabled();
-      expect(isDirty).toBe(true);
+      expect(isFormDirty).toBe(true);
     });
 
     // text input is enabled and is now editable so we change value
     fireEvent.change(textInput, {target: {value: 'foo'}});
 
     // form should be still dirty
-    await waitFor(() => expect(isDirty).toBe(true));
+    await waitFor(() => expect(isFormDirty).toBe(true));
 
     // user returns checkbox's value to defaultValue which disables textInput
     fireEvent.click(checkbox);
@@ -103,7 +108,7 @@ describe('formState', () => {
 
       expect(textInput).toBeDisabled();
 
-      expect(isDirty).toBe(false);
+      expect(isFormDirty).toBe(false);
     });
   });
 });
